461ca4bd299d14763504ab6e4674ae6fc8f828d2
Rafael Matoso <rafaelmatosog@gmail.com>
2021-01-01 16:34:22 -0300

    Refatoracao do tipo ApplicationState

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,10 @@
 import { applyMiddleware, createStore, Store } from 'redux';
 import createSagaMiddleware from 'redux-saga';
-import { RepositoriesState } from './ducks/repositories/types';
 
 import rootReducer from './ducks/rootReducer';
 import rootSaga from './ducks/rootSaga';
 
-export interface ApplicationState {
-  repositories: RepositoriesState;
-}
+export type ApplicationState = ReturnType<typeof rootReducer>;
 
 const sagaMiddleware = createSagaMiddleware();
 
