d13831a10d167fb44110612088b04b94a9b2d171
Rafael Matoso <rafaelmatosog@gmail.com>
2021-01-09 13:33:31 -0300

    RepositorList refatorado e escutando o estado loading

--- src/components/RepositoryList/index.tsx
+++ src/components/RepositoryList/index.tsx
@@ -1,22 +1,28 @@
 import { useSelector } from 'react-redux';
+import ReactLoading from 'react-loading';
 import { ApplicationState } from '../../store';
-import { Repository } from '../../store/ducks/repositories/types';
 import RepositoryItem from '../RepositoryItem';
 
 const RepositoryList: React.FC = () => {
   const repositories = useSelector((state: ApplicationState) => state.repositories);
 
-  const { data } = repositories;
+  const { data, loading } = repositories;
 
-  const renderRepositoryItem = (repository: Repository) => (
-    <RepositoryItem key={repository.id} repository={repository} />
+  const renderLoading = () => (
+    <ReactLoading type="spin" color="#000000" height={50} width={50} />
+  );
+
+  const renderRepositoryList = () => (
+    <ul>
+      {data.map((repository) => (
+        <RepositoryItem key={repository.id} repository={repository} />
+      ))}
+    </ul>
   );
 
   return (
     <>
-      <ul>
-        {data.map((repository) => renderRepositoryItem(repository))}
-      </ul>
+      {loading ? renderLoading() : renderRepositoryList()}
     </>
   );
 };
