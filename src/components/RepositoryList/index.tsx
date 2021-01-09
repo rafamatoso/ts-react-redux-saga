50694a1c1d322369926df79ec6a89e20bc02504e
Rafael Matoso <rafaelmatosog@gmail.com>
2021-01-09 14:30:44 -0300

    Refatoracao no css de RepositoryList

--- src/components/RepositoryList/index.tsx
+++ src/components/RepositoryList/index.tsx
@@ -3,21 +3,27 @@ import ReactLoading from 'react-loading';
 import { ApplicationState } from '../../store';
 import RepositoryItem from '../RepositoryItem';
 
+import './styles.scss';
+
 const RepositoryList: React.FC = () => {
   const repositories = useSelector((state: ApplicationState) => state.repositories);
 
   const { data, loading } = repositories;
 
   const renderLoading = () => (
-    <ReactLoading type="spin" color="#000000" height={50} width={50} />
+    <div id="loading-container">
+      <ReactLoading type="spin" color="#000000" height={50} width={50} />
+    </div>
   );
 
   const renderRepositoryList = () => (
-    <ul>
+
+    <ul id="list-container">
       {data.map((repository) => (
         <RepositoryItem key={repository.id} repository={repository} />
       ))}
     </ul>
+
   );
 
   return (
