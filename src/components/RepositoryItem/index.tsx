1c985aafc0f1adc17b9ffd20025200905ac8daa7
Rafael Matoso <rafaelmatosog@gmail.com>
2021-01-09 21:39:27 -0300

    Componente RepositoryItem atualizado com novas regras de negocio

--- src/components/RepositoryItem/index.tsx
+++ src/components/RepositoryItem/index.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable camelcase */
 import { memo } from 'react';
+import { icon } from '../../assets/icons';
 import { Repository } from '../../store/ducks/repositories/types';
 
 import './styles.scss';
@@ -7,13 +9,34 @@ interface OwnProps {
     repository: Repository;
 }
 
-const RepositoryItem: React.FC<OwnProps> = ({ repository }: OwnProps) => (
-  <>
-    <div id="item-container">
-      <li>{repository.name}</li>
-      <li>{repository.stargazers_count}</li>
-    </div>
-  </>
-);
+const RepositoryItem: React.FC<OwnProps> = ({ repository }: OwnProps) => {
+  const { name, stargazers_count, html_url } = repository;
+
+  const goToRepository = (url: string) => {
+    window.open(url, '_blank');
+  };
+
+  return (
+    <>
+      <div
+        id="item-container"
+        onClick={() => goToRepository(html_url)}
+        onKeyDown={() => goToRepository(html_url)}
+        role="button"
+        tabIndex={0}
+      >
+        <li>{name}</li>
+        <div id="star-container">
+          <img
+            className="star-icon"
+            src={icon.star}
+            alt="Star"
+          />
+          <li className="star-count">{stargazers_count > 0 ? stargazers_count : ''}</li>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default memo(RepositoryItem);
