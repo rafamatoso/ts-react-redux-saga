4f56bb63d1cd44989869f5db48807c2ad79ba778
Rafael Matoso <rafaelmatosog@gmail.com>
2021-01-10 22:56:46 -0300

    Adiçao de parametro meta na action loadRequest

--- src/store/ducks/repositories/actions.ts
+++ src/store/ducks/repositories/actions.ts
@@ -2,8 +2,8 @@
 import { Action, action } from 'typesafe-actions';
 import { RepositoriesTypes, Repository } from './types';
 
-export const loadRequest = (username: string): Action =>
-  action(RepositoriesTypes.LOAD_REQUEST, { username });
+export const loadRequest = (username: string, meta: unknown): Action =>
+  action(RepositoriesTypes.LOAD_REQUEST, { username, meta });
 
 export const loadSuccess = (data: Repository[]): Action =>
   action(RepositoriesTypes.LOAD_SUCCESS, { data });
