de59e82edeeff060f63ba74ce199681153a0db23
Rafael Matoso <rafaelmatosog@gmail.com>
2021-01-10 23:21:27 -0300

    Adicao de nova rota com configuracao

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,16 @@
 import {
   Switch, BrowserRouter, Redirect, Route,
 } from 'react-router-dom';
-import RepositoryList from '../components/RepositoryList';
-
 import Search from '../pages/Search';
+import RepositoryList from '../components/RepositoryList';
+import NotFound from '../pages/NotFound';
 
 export const Routes: React.FC = () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" exact component={Search} />
       <Route path="/result" exact component={RepositoryList} />
+      <Route path="/notfound" exact component={NotFound} />
       <Route path="/" exact component={() => <Redirect to="/search" />} />
     </Switch>
   </BrowserRouter>
