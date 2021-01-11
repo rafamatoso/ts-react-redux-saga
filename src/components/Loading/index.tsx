93c5d6afb8236ffe275b58818e951fcf495a9682
Rafael Matoso <rafaelmatosog@gmail.com>
2021-01-10 23:03:36 -0300

    Criacao de componente Loading para melhor reutilizaçao

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,9 @@
+import ReactLoading from 'react-loading';
+
+import './styles.scss';
+
+export const Loading = () => (
+  <div id="loading-container">
+    <ReactLoading type="spin" color="#000000" height={50} width={50} />
+  </div>
+);
