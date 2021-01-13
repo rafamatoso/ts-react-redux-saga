c7e0caaf8f98b167173a9c888c3082bc70649d21
Rafael Matoso <rafaelmatosog@gmail.com>
2021-01-12 22:34:16 -0300

    Adição da interface Owner

--- src/store/ducks/repositories/types.ts
+++ src/store/ducks/repositories/types.ts
@@ -12,11 +12,17 @@ export enum RepositoriesTypes {
 /**
  * Data types
  */
+export interface Owner {
+  avatar_url: string;
+  login: string;
+}
+
 export interface Repository {
   id: number;
   name: string;
   html_url: string;
   stargazers_count: number;
+  owner: Owner;
 }
 
 /**
