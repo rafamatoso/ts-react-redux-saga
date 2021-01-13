b7fe0b60fc85de398b2d8dec461100c0d5054bc0
Rafael Matoso <rafaelmatosog@gmail.com>
2021-01-12 22:33:23 -0300

    Refatoração e melhorias no componente NotFound

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/interactive-supports-focus */
 import { useHistory } from 'react-router-dom';
+import { icon } from '../../assets/icons';
 import './styles.scss';
 
 const NotFound: React.FC = () => {
@@ -12,14 +14,34 @@ const NotFound: React.FC = () => {
   };
 
   return (
-    <div
-      id="not-found-container"
-      onClick={backToSearch}
-      onKeyDown={backToSearch}
-      role="button"
-    >
-      <h1 className="title">:o Usuário não encontrado :0</h1>
-    </div>
+    <>
+      <div id="not-found-container">
+        <div id="not-found-msg-container">
+          <button
+            className="previous-button"
+            onClick={backToSearch}
+            onKeyDown={backToSearch}
+            type="button"
+          >
+            <img
+              className="previous-image"
+              src={icon.previous}
+              alt="Previous Page"
+            />
+          </button>
+          <h1 className="title">Usuário não encontrado</h1>
+        </div>
+        <div id="footer-container">
+          Icons made by
+          {' '}
+          <a href="https://www.flaticon.com/authors/smartline" title="Smartline">Smartline</a>
+          {' '}
+          from
+          {' '}
+          <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+        </div>
+      </div>
+    </>
   );
 };
 
