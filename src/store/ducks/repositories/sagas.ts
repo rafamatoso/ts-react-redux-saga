d53be5e6822b95745cefa0949be3b585cb522219
Rafael Matoso <rafaelmatosog@gmail.com>
2021-01-15 21:24:39 -0300

    Refatoração no sagas load para o mesmo retornar todos os repositórios do usuário

--- src/store/ducks/repositories/sagas.ts
+++ src/store/ducks/repositories/sagas.ts
@@ -1,18 +1,42 @@
+/* eslint-disable camelcase */
 import { call, Effect, put } from 'redux-saga/effects';
 import api from '../../../services/api';
 import { loadFailure, loadSuccess } from './actions';
 
+interface DataResponse {
+  public_repos: number;
+}
+
+interface UserResponse {
+  data: DataResponse;
+}
+
+interface ReposResponse {
+  data: any;
+}
+
 export function* load(action: Effect) {
+  const { username, meta } = action.payload;
+
   try {
-    const response = yield call(
+    const userResponse: UserResponse = yield call(api.get, `users/${username}`);
+
+    const { public_repos } = userResponse.data;
+
+    const response: ReposResponse = yield call(
       api.get,
-      `users/${action.payload.username}/repos`,
+      `users/${username}/repos`,
+      {
+        params: {
+          per_page: public_repos,
+        },
+      },
     );
 
     yield put(loadSuccess(response.data));
-    action.payload.meta('/result');
+    meta('/result');
   } catch (error) {
     yield put(loadFailure());
-    action.payload.meta('/notfound');
+    meta('/notfound');
   }
 }
