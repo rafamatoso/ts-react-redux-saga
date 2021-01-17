6894dd6a8f7342c5af46b69495196c86f9b10a56
Rafael Matoso <rafaelmatosog@gmail.com>
2021-01-17 19:09:08 -0300

    Componente RepositoryList mudado para Result e agora pertence a pasta pages

---
+++ src/pages/Result/index.tsx
@@ -0,0 +1,39 @@
+import { useSelector } from 'react-redux';
+import { ApplicationState } from '../../store';
+import RepositoryItem from '../../components/RepositoryItem';
+import { Loading } from '../../components/Loading';
+import './styles.scss';
+
+const Result: React.FC = () => {
+  const repositories = useSelector((state: ApplicationState) => state.repositories);
+
+  const { data, loading } = repositories;
+
+  const { owner } = data[0];
+
+  const renderRepositoryList = () => (
+    <>
+      <div id="owner-container">
+        <div id="img-container">
+          <img src={owner.avatar_url} alt="Avatar" />
+        </div>
+        <h1>{owner.login}</h1>
+      </div>
+      <ul id="list-container">
+        {data
+          .sort((previous, current) => current.stargazers_count - previous.stargazers_count)
+          .map((repository) => (
+            <RepositoryItem key={repository.id} repository={repository} />
+          ))}
+      </ul>
+    </>
+  );
+
+  return (
+    <>
+      {loading ? Loading() : renderRepositoryList()}
+    </>
+  );
+};
+
+export default Result;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,14 +3,14 @@ import {
 } from 'react-router-dom';
 import Route from './routeWrapper';
 import Search from '../pages/Search';
-import RepositoryList from '../components/RepositoryList';
+import Result from '../pages/Result';
 import NotFound from '../pages/NotFound';
 
 export const Routes: React.FC = () => (
   <BrowserRouter>
     <Switch>
       <Route path="/home" exact component={Search} />
-      <Route path="/result" exact component={RepositoryList} isPrivate />
+      <Route path="/result" exact component={Result} isPrivate />
       <Route path="/notfound" exact component={NotFound} />
       <Route path="/" exact component={() => <Redirect to="/home" />} />
     </Switch>
