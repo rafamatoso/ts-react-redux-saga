ce02a961e2a84a15897847904b546d4d2353ee2b
Rafael Matoso <rafaelmatosog@gmail.com>
2021-01-16 22:31:00 -0300

    Inclusão do reducer de tema no rootReducer

--- src/store/ducks/rootReducer.ts
+++ src/store/ducks/rootReducer.ts
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 
+import theme from './theme';
 import repositories from './repositories';
 
-export default combineReducers({ repositories });
+export default combineReducers({ theme, repositories });
